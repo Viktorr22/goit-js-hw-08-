d7e3d18a5337d03b4aa4bc5901de9dbd0a101856
Viktorr22 <110117605+Viktorr22@users.noreply.github.com>
2022-12-23 18:31:51 +0200

    dz8 task1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,7 +1,31 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+console.log(SimpleLightbox);
 
 console.log(galleryItems);
+const galleryList = document.querySelector('.gallery');
+const galleryItem = galleryItems.map((galItem) => {
+    return `<a class="gallery__item" href="${galItem.original}"> 
+        <img
+          class="gallery__image"
+          src="${galItem.preview}"
+          data-source="${galItem.original}"
+          alt="${galItem.description}"
+          
+        />
+      </a>`;
+}).join('');
+galleryList.insertAdjacentHTML('afterbegin', galleryItem);
+
+var lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+});
+
+
+
 
 
