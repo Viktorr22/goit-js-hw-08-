02b50e3e7b5b37e2ef6faa270efc7f5fb77f48a6
Viktorr22 <110117605+Viktorr22@users.noreply.github.com>
2022-12-24 11:56:40 +0200

    task2done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,47 @@
+import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
+
+const CURRENT_TIME = "videoplayer-current-time";
+
+
+
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+const onPlay = function(data) {
+    localStorage.setItem(CURRENT_TIME, JSON.stringify(data));
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+
+try {
+    const savedSettings = localStorage.getItem(CURRENT_TIME);
+    const parsedSettings = JSON.parse(savedSettings);
+    const playTime = parsedSettings.seconds;
+    player.setCurrentTime(playTime).then(function(seconds) {
+   
+    }).catch(function(error) {
+        switch (error.name) {
+        case 'RangeError':           
+            break;
+        default:            
+            break;
+    }
+});
+}
+catch (error) {
+    console.log(error.name);
+};
+
+
+
+
+
+
+
+
+
+
+
 
